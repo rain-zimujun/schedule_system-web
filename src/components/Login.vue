17db56d175f894c06c3a514cdfba918de31ff3c8
rain-zimujun <1738388256@qq.com>
2026-04-18 15:59:20 +0800

    submit2:实现了登录功能

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,36 +1,73 @@
 <script setup>
- 
-import { reactive,ref } from 'vue';
-let loginUser = reactive({
-    username : '',
-    userPwd : ''
-})
-
-let usernameMsg = ref('');
-let userPwdMsg = ref('');
-
-function checkUsername(){
-    let usernameReg = /^[a-zA-Z0-9]{5,10}$/;
-    if(!usernameReg.test(loginUser.username)){
-        usernameMsg.value = '格式有误';
-        return false;
-    }
-    usernameMsg.value = 'OK';
-    return true;
     
-}
+    import { reactive,ref } from 'vue';
+    let loginUser = reactive({
+        username : '',
+        userPwd : ''
+    })
+    import {useRouter} from 'vue-router'
+    import request from '../utils/request'
 
-function checkUserPwd(){
-    let userPwdReg = /^[0-9]{6}$/
+    const router = useRouter()
 
-    if(!userPwdReg.test(loginUser.userPwd)){
-        userPwdMsg.value = '格式有误';
-        return false;
+
+    let usernameMsg = ref('');
+    let userPwdMsg = ref('');
+
+    function checkUsername(){
+        let usernameReg = /^[a-zA-Z0-9]{5,10}$/;
+        if(!usernameReg.test(loginUser.username)){
+            usernameMsg.value = '格式有误';
+            return false;
+        }
+        usernameMsg.value = 'OK';
+        return true;
+        
     }
-    userPwdMsg.value = 'OK';
-    return true;
-    
-}
+
+    function checkUserPwd(){
+        let userPwdReg = /^[0-9]{6}$/
+
+        if(!userPwdReg.test(loginUser.userPwd)){
+            userPwdMsg.value = '格式有误';
+            return false;
+        }
+        userPwdMsg.value = 'OK';
+        return true;
+        
+    }
+
+    //登录功能
+    async function login(){
+        //表单数据都正确再进行提交,减轻后端压力
+        let flag1 = checkUsername();
+        let flag2 = checkUserPwd();
+        if(!(flag1 && flag2)){
+            return;
+        }
+
+        let {data} = await request.post('user/login',loginUser)
+        if(data.code == 200){
+            //登录成功,跳转到showschedule页面
+            alert('登录成功!')
+            router.push('/showSchedule')
+        }else if(data.code == 501){
+            alert('用户名错误')
+        }else if(data.code == 503){
+            alert('密码错误')
+        }else{
+            alert('未知错误')
+        }
+    }
+
+
+    //重置功能
+    function reset(){
+        usernameMsg.value = '';
+        userPwdMsg.value = '';
+        loginUser.username = '';
+        loginUser.userPwd = '';
+    }
 
 </script>
 
@@ -60,8 +97,8 @@ function checkUserPwd(){
             </tr>
             <tr class="ltr">
                 <td colspan="2" class="buttonContainer">
-                    <input class="btn1" type="button" value="登录">
-                    <input class="btn1" type="button" value="重置">
+                    <input class="btn1" type="button" @click="login()" value="登录">
+                    <input class="btn1" type="button" @click="reset()" value="重置">
                     <router-link to="/regist">
                       <button class="btn1">去注册</button>
                     </router-link>
--- src/router/router.js
+++ src/router/router.js
@@ -10,7 +10,7 @@ const router = createRouter({
     routes : [
         {
             path : '/',
-            component : ShowSchedule
+            component : Login
         },
         {
             path : '/showSchedule',
